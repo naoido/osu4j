8f540194fb5c48eef16ec708187ebb8831a97c62
なおいど <54303857+nqzu@users.noreply.github.com>
2022-06-08 09:53:38 +0900

    Exampleコードの追加

--- src/test/java/com/naoido/example/Example.java
+++ src/test/java/com/naoido/example/Example.java
@@ -1,6 +1,7 @@
 package com.naoido.example;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.naoido.osu.beatmap.Beatmap;
 import com.naoido.osu.user.*;
 
 import com.naoido.osu.api.OsuApiResponse;
@@ -20,6 +21,20 @@ public class Example {
     public static void main(String[] args) throws OsuException, JsonProcessingException {
         Osu osu = new OsuBuilder(CLIENT_ID, CLIENT_SECRET).build();
 
+        System.out.println("---------------");
+
+        UserBeatmapScore userBeatmapScore = osu.getUserBeatmapScore(2480902, 9144414).getUserBeatmapScore();
+
+        User naoido = userBeatmapScore.getUser();
+        Score score = userBeatmapScore.getScore();
+        Beatmap beatmap = userBeatmapScore.getBeatmap();
+        System.out.println("iconURL: " + naoido.getAvatarUrl());
+        System.out.println("Score: " + score.getScore());
+        System.out.println("PP: " + score.getPerformancePoint());
+        System.out.println("BeatmapURL: " + beatmap.getUrl());
+
+        System.out.println("---------------");
+
         OsuApiResponse res = osu.getUser("Naoido");
         User user = res.getUser();
         UserStatistics statistics = user.getStatistics();
@@ -32,5 +47,7 @@ public class Example {
         for (Map.Entry<String, Integer> rank: statistics.getGradeCounts().getTreeMap().entrySet()) {
             System.out.println(rank.getKey() + ": " + rank.getValue());
         }
+
+        System.out.println("---------------");
     }
 }
