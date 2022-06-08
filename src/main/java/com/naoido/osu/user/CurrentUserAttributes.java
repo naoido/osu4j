562afc22e6a5edaf37b06597842ef5db673d3849
なおいど <54303857+nqzu@users.noreply.github.com>
2022-06-08 09:53:19 +0900

    新規クラス pinがnullしか返って来なかったため型がわからずObject

---
+++ src/main/java/com/naoido/osu/user/CurrentUserAttributes.java
@@ -0,0 +1,11 @@
+package com.naoido.osu.user;
+
+public class CurrentUserAttributes {
+    private Object pin;
+
+    public CurrentUserAttributes() {}
+
+    public Object getPin() {
+        return this.pin;
+    }
+}
